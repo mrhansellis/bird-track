fc241e3fff1335cf0cc4939e479268aaeb871699
Hans Ellis <bullhorn@gmail.com>
2022-10-24 20:22:01 -0700

    instantiated map object and installed google maps api

---
+++ src/service/mapsAPI.js
@@ -0,0 +1,13 @@
+import { Loader } from "@googlemaps/js-api-loader";
+
+const loader = new Loader({
+  apiKey: `${process.env.MAPS_KEY}`,
+  version: "weekly"
+});
+
+loader.load().then(() => {
+  map = new google.maps.Map(document.getElementById('map'), {
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
+  });
+});
