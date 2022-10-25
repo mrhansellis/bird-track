35e5e17d1999d0ab5a6fc1cac499c792fdd0a8e4
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-24 19:56:21 -0700

    Built webpack environment
    Co-authored-by: Bai Jaitrong <bjaitrong22@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery-csv';
-
-/* eslint-disable no-unused-vars */
-
-let csv = require('jquery-csv');
-
-$.csv.toObjects(csv);
-
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Currency-Exchange',
+      title: 'Bird Tracker',
       template: './src/index.html',
       inject: 'body'
     }),
