2ca8c5bb80d707c8189df729cc93633769f042b6
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-24 19:55:14 -0700

    removed the csv file and instead parsed the data into an array. Co-authored-by: Bai Jaitrong <bd2jaitrong@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery-csv';
+
+/* eslint-disable no-unused-vars */
+
+let csv = require('jquery-csv');
+
+$.csv.toObjects(csv);
+
