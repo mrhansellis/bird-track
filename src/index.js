eb2acd69f9884da6d031428da117d758d58adfb3
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-24 20:38:20 -0700

    "Moved species-code.js to js/. Updated input handling. Co-authored-by: Bai Jaitrong <bd2jaitrong@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './js/species-code.js';
+import SpeciesCode from './js/species-code.js';
+
+let commonName = "common ostrich";
+let species1 = new SpeciesCode();
+console.log(species1.getSpeciesCode(commonName.toUpperCase()));
