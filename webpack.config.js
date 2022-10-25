d1984cdd096951f2581a650c0fe37b40fb2770f2
Rafael Petrachini <raffp@me.com>
2022-10-24 20:40:53 -0700

    add file loader and html loader packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,51 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Currency-Exchange',
-      template: './src/index.html',
-      inject: 'body'
+      title: "Currency-Exchange",
+      template: "./src/index.html",
+      inject: "body",
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
 };
