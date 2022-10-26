dfbe44adc394cd9fb95b4800d80b6bf954f1251a
Hans Ellis <bullhorn@gmail.com>
2022-10-25 19:17:43 -0700

    add css styling and image handling to webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+/*import { Loader } from "@googlemaps/js-api-loader";
+
+const loader = new Loader({
+  apiKey: `${process.env.MAPS_KEY}`,
+  version: "weekly"
+});
+
+/*loader.load().then(() => {
+  //map = new google.maps.Map(document.getElementById('map'), {
+  //center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
+  });
+});*/
--- src/service/mapsAPI.js
+++ src/service/mapsAPI.js
@@ -1,13 +1,2 @@
 import { Loader } from "@googlemaps/js-api-loader";
 
-const loader = new Loader({
-  apiKey: `${process.env.MAPS_KEY}`,
-  version: "weekly"
-});
-
-loader.load().then(() => {
-  map = new google.maps.Map(document.getElementById('map'), {
-  center: { lat: -34.397, lng: 150.644 },
-  zoom: 8,
-  });
-});
