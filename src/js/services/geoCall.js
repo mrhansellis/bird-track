a5263ffd2d436125f9e8861fc37917c9cd89ceac
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-30 19:11:38 -0400

    return lat and long result as array
    Co-authored-by: Bai  Jaitrong <bd2jaitrong@gmail.com>
    Co-authored-by: David Marchuk <davormarchuk@gmail.com>
    Co-authored by: Hans Ellis <bullhorn@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>

--- src/js/services/geoCall.js
+++ src/js/services/geoCall.js
@@ -5,15 +5,18 @@ export default class GeoCall {
 //when the button is checked to manual, a text input field appears and whatever is entered is passed here as radioManualTxt
 // in HTML radio
 //in geograb call on index.js pass this checked radio button value as either 'ip' or 'manual'
+    let url;
     if (radioBtnVal === 'ip') {
-      const url = `https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.MAPS_KEY}`;
+      url = `https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.MAPS_KEY}`;
     } else if (radioBtnVal === 'manual') {
       const userAddressApiArg = radioManualTxt.split('').join('+');
-      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddressApiArg}&key=${process.env.MAPS_KEY}`;
+      url = `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddressApiArg}&key=${process.env.MAPS_KEY}`;
     } else {
       //an error if the user manages to break the radio button binary choice
     }
-    
+    console.log('url in GeoGrab call: ');
+    console.log(url);
+
     fetch(url, {
       method: "POST"
     })
@@ -22,15 +25,27 @@ export default class GeoCall {
           const errorMessage = `${response.status} ${response.statusText}`;
           throw new Error(errorMessage);
         } else {
-          return response.json();
+          const jsonResponse = response.json();
+          return jsonResponse;
         }
       })
-      .catch((error) => {
-        return error;
-      })
-      .then((location) => {
-        console.log(location);
-        return location;
-      });
+      
+      .then((jsonResponse) => {
+      let location;
+      if (radioBtnVal === 'ip') {
+        location = [jsonResponse['location']['lat'], jsonResponse['location']['lng']];
+      } else if (radioBtnVal === 'manual') {
+        location = [jsonResponse.results[0].geometry.location.lat, jsonResponse.results[0].geometry.location.lng];
+      }
+      return location;
+    })
+
+    .catch((error) => {
+      return error;
+    })
+    .then((location) => {
+      console.log(location);
+      return location;
+    });
   }
 }
