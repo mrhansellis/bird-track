9588a3fb607769ae095d199073392ef2043ffa95
DaveMarchuk <110414567+DaveMarchuk@users.noreply.github.com>
2022-10-30 10:20:08 -0700

    Updated code to add a dropdown menu. Co-authored-by: Bai  Jaitrong <bd2jaitrong@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { getAPIData } from './js/get-apidata.js';
 
-let commonName = "House Sparrow".toLowerCase();
-getAPIData(commonName).then((results) => console.log(results));
+getAPIData();
--- src/js/get-apidata.js
+++ src/js/get-apidata.js
@@ -1,7 +1,86 @@
 import { BirdTrackerService } from './services/bird-track-service.js';
-import { BirdLocationService } from './services/bird-location-service.js';
+//import { BirdLocationService } from './services/bird-location-service.js';
 
-export function getAPIData(comNameInput) {
+let birdNames = [];
+const birdNameInputElement = document.querySelector("#birdName-input");
+
+function getAPIData() {
+  BirdTrackerService.getSpeciescode()
+    .then(function(birdTrackerResponse) {
+      if (birdTrackerResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the bird data from eBird API,
+        Status code: ${birdTrackerResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      birdNames = birdTrackerResponse.map((bird) => {
+        return bird.comName;
+      });
+      birdNames.sort();            
+    })
+    .catch(function(error) {
+      printError(error);
+    });
+}
+
+function onKeyInputChange() {
+  
+  removeAutoDropDown ();
+  const filteredBirdNames = [];
+  const value = birdNameInputElement.value.toLowerCase();
+
+  if(value.lenght === 0) {
+    return;
+  } 
+  
+  if(value.length > 2) {
+
+    birdNames.forEach((birdName) => {
+      if(birdName.toLowerCase().includes(value)) {
+        filteredBirdNames.push(birdName); 
+      }
+    });
+  }
+  
+  createAutoCompleteDropDown(filteredBirdNames);
+}
+
+function createAutoCompleteDropDown(nameList) {
+  const listElement = document.createElement("ul");
+  listElement.className = "autocomplete-list";
+  listElement.id = "autocomplete-list";
+
+  nameList.forEach((birdName) => {
+    const listItem = document.createElement("li");
+    const birdNameBtn = document.createElement("button");
+    birdNameBtn.innerHTML = birdName;
+    birdNameBtn.addEventListener("click", onBirdButtonClick);
+    listItem.appendChild(birdNameBtn);
+    listElement.appendChild(listItem); 
+  });
+  document.querySelector("#autocomplete-list-div").appendChild(listElement);
+} 
+
+function removeAutoDropDown () {
+  const listElement = document.querySelector("#autocomplete-list");
+  if(listElement) listElement.remove();
+}
+
+function onBirdButtonClick(e) {
+  e.preventDefault();
+  const buttonE1 = e.target;
+  birdNameInputElement.value = buttonE1.innerHTML;
+  removeAutoDropDown();
+}
+
+function printError(error) {
+  document.querySelector('#error').innerText = error;
+}
+
+birdNameInputElement.addEventListener("input", onKeyInputChange);
+
+getAPIData();
+
+/*export function getAPIData(comNameInput) {
   return BirdTrackerService.getSpeciescode()
     .then(function(birdTrackerResponse) {
       if (birdTrackerResponse instanceof Error) {
@@ -61,4 +140,4 @@ export function getBirdLocation(speciesCodeConv)  {
   .catch(function(error)  {
     console.log(error);
   });
-}
+}*/
